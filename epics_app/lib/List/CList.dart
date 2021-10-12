e106b82b6da4292bce3f85400834b97b646bf59f
Scorpion <scorupi819@outlook.com>
2021-10-12 09:00:19 -0700

    10-12-21_ListViewed

--- epics_app/lib/List/CList.dart
+++ epics_app/lib/List/CList.dart
@@ -2,17 +2,20 @@
 
 import 'package:flutter/material.dart';
 
+List<Contact> ContactL = //contact list
+[];
+
 class Contact
 {
   //name
-  var PName = ""; //primero name
-  var MI    = ""; // middle intial
+  var PName = ""; //primero name/first name
+  var MInitial    = ""; // middle intial
   var LName = ""; // last name
   var FName = ""; // full name
 
-	void UFull()
+	void UFull() //update full name
 	{
-		FName = PName + " " + MI + " " + LName;
+		FName = PName + " " + MInitial + " " + LName;
 	}
 
   //phone number
@@ -24,27 +27,66 @@ class Contact
   bool    BothM = false;  //Both message types
   bool TMessage = false;  //Text message
   bool CMessage = false;  // call as message
+
+  Contact(String PN, String MI, String LN, int CPN1, int CPN2, int CPN3, bool B, bool TM)
+  {
+    PName = PN;
+    MInitial = MI;
+    LName = LN;
+    UFull();
+
+    PN1 = CPN1;
+    PN2 = CPN2;
+    PN3 = CPN3;
+
+    BothM = B;
+    TMessage = B || TM;
+    CMessage = B || !TM;
+
+  }
+
 }
 
-Container GenerateLE(Contact LC)	// generate list entry
+ListView CreateList(List<Contact> Contacts, BuildContext context)
 {
-	// ignore: sized_box_for_whitespace
-	return Container
-	(
-		height: 50,
-		child: Center(child: Text(LC.FName)),
-		
-		
-		/*child: const Center
-		(
-			child: FloatingActionButton
-			(
-				child: Icon(CEDIT),
-			),
-		)*/
+  return ListView.builder
+  (
+    itemCount: Contacts.length,
 
-	);
+    itemBuilder: (BuildContext context, int index)
+    {
+      return Text(Contacts[index].FName);
+    },
+  );
 }
+
+ListView GTL(int TN, BuildContext context) //generate test list
+{
+  String PName = "Test";
+  String MI = "I";
+  String LName = "ng";
+
+  int PN1 = 0;
+  int PN2 = 0;
+  int PN3 = 0;
+
+  bool Both = false;
+  bool Text = true;
+
+  for (var fv1 = 0; fv1 < TN; fv1++)
+  {
+    PN3++;
+    PN2 += ((PN3 > 9999) ? 1: 0); 
+    PN1 += ((PN2 > 999) ? 1 : 0); 
+    PN3 = PN3 % 10000;
+    ContactL.add(Contact(PName, MI, LName, PN1, PN2, PN3, Both, Text));
+  }
+
+  return CreateList(ContactL, context);
+
+}
+
+
 
 //icons
 const CEDIT   = Icons.edit;           // contact edit
