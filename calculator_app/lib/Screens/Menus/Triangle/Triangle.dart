310b0f710dac66dbb84fa4e847ecca2396992f63
Andrew-Null <scorupi819@outlook.com>
2022-05-10 08:15:21 -0700

    5-10-22_SAAyASS

--- calculator_app/lib/Screens/Menus/Triangle/Triangle.dart
+++ calculator_app/lib/Screens/Menus/Triangle/Triangle.dart
@@ -9,12 +9,9 @@ import '../../../Geometry/Triangle/Triangle.dart' as Tri;
 import 'TriVars.dart' as TV;
 import 'TriComponents.dart' as TC;
 
-//import 'dart:io' as DIO;
 
 int Iterate(int val, TV.Contents mode)
 {
-  //print("Iterating $val for $mode");
-  //Future.delayed(Duration(seconds: 10), () => null);
   switch (mode)
   {
     case TV.Contents.None:
@@ -29,7 +26,6 @@ int Iterate(int val, TV.Contents mode)
         case 4: {return 0;}
         default: 
         {
-          //print("None defaulted with $val");
           return Iterate((val % 6).abs(), mode);
         }
       }
@@ -43,7 +39,6 @@ int Iterate(int val, TV.Contents mode)
         case 0: {return 5;} case 4: {return 5;}
         default: 
         {
-          //print("Side defaulted with $val");
           return Iterate(val.abs() % 6, mode);
         }
       }
@@ -57,7 +52,6 @@ int Iterate(int val, TV.Contents mode)
         case 2: {return 0;} case 4: {return 0;}
         default: 
         {
-          //print("Angle defaulted with $val");
           return Iterate(val.abs() % 6, mode);
         }
       }
@@ -68,9 +62,10 @@ int Iterate(int val, TV.Contents mode)
 Tri.Triangle? TryAAS(int A1, {int limit = 3})
 {
   print("Trying AAS $limit");
-  //DIO.sleep(Duration(seconds: 10));
   int A2 = Iterate(A1, TV.Contents.Angle);
   int S = Iterate(A2, TV.Contents.Side);
+
+  print("${TV.TxtControllers[A1].text} : ${TV.TxtControllers[A2].text} : ${TV.TxtControllers[S].text}");
 
   if (TV.Values[A1] is Ang.Angle && TV.Values[A2] is Ang.Angle && TV.Values[S] is Side.Side)
   {
@@ -85,7 +80,11 @@ Tri.Triangle? TryAAS(int A1, {int limit = 3})
 
   if (limit > 0)
   {
-    return TryAAS(Iterate(A1, TV.Contents.Angle), limit: limit - 1);
+    var irt = TryAAS(Iterate(A1, TV.Contents.Angle), limit: limit - 1);
+    if (irt is Tri.Triangle)
+    {
+      irt.RotAaB();
+    }
   }
   return null;
 }
@@ -93,7 +92,6 @@ Tri.Triangle? TryAAS(int A1, {int limit = 3})
 Tri.Triangle? TryASA(int A1, {int limit = 3})
 {
   print("Trying ASA $limit");
-  //DIO.sleep(Duration(seconds: 10));
   int S = Iterate(A1, TV.Contents.Side);
   int A2 = Iterate(S, TV.Contents.Angle);
 
@@ -110,15 +108,60 @@ Tri.Triangle? TryASA(int A1, {int limit = 3})
 
   if (limit > 0)
   {
-    return TryASA(Iterate(A1, TV.Contents.Angle), limit: limit - 1);
+    var irt = TryASA(Iterate(A1, TV.Contents.Angle), limit: limit - 1);
+    if (irt is Tri.Triangle)
+    {
+      irt.RotAaB();
+    }
   }
   return null;
 }
 
+Tri.Triangle? TryASS(int A, {int limit = 3})
+{
+  int S1 = Iterate(A, TV.Contents.Side);
+  int S2 = Iterate(S1, TV.Contents.Side);
+
+  Ang.Angle? a = TV.Values[A] as Ang.Angle?;
+  Side.Side? s1 = TV.Values[S1] as Side.Side?;
+  Side.Side? s2 = TV.Values[S2] as Side.Side?;
+
+  if (a != null && s1 != null && s2 != null)
+  {
+    return Tri.Triangle.SSA(s2, s1, a);
+  }
+  if (limit > 0)
+  {
+    return TryASS(Iterate(A, TV.Contents.Angle), limit: limit - 1);
+  }
+  return null;
+
+}
+
+Tri.Triangle? TrySAA(int S, {int limit = 3})
+{
+  int A1 = Iterate(S, TV.Contents.Angle);
+  int A2 = Iterate(A1, TV.Contents.Angle);
+
+  Side.Side? s = TV.Values[S] as Side.Side?;
+  Ang.Angle? a1 = TV.Values[A1] as Ang.Angle?;
+  Ang.Angle? a2 = TV.Values[A2] as Ang.Angle?;
+
+  if (s != null && a1 != null && a2 != null)
+  {
+    return Tri.Triangle.AAS(a2, a1, s);
+  }
+  if (limit > 0)
+  {
+    return TrySAA(Iterate(S, TV.Contents.Side), limit: limit - 1);
+  }
+  return null;
+
+}
+
 Tri.Triangle? TrySSA(int S1, {int limit = 3})
 {
-  print("Trying SSA $limit");
-  //DIO.sleep(Duration(seconds: 10));
+  //print("Trying SSA $limit");
   int S2 = Iterate(S1, TV.Contents.Side);
   int A = Iterate(S2, TV.Contents.Angle);
 
@@ -135,15 +178,18 @@ Tri.Triangle? TrySSA(int S1, {int limit = 3})
 
   if (limit > 0)
   {
-    return TrySSA(Iterate(S1, TV.Contents.Side), limit: limit - 1);
+    var irt = TrySSA(Iterate(S1, TV.Contents.Side), limit: limit - 1);
+    if (irt is Tri.Triangle)
+    {
+      irt.RotAaB();
+    }
   }
   return null;
 }
 
 Tri.Triangle? TrySAS(int S1, {int limit = 3})
 {
-  print("Trying SAS $limit");
-  //DIO.sleep(Duration(seconds: 10));
+  //print("Trying SAS $limit");
   int A = Iterate(S1, TV.Contents.Angle);
   int S2 = Iterate(A, TV.Contents.Side);
 
@@ -160,11 +206,12 @@ Tri.Triangle? TrySAS(int S1, {int limit = 3})
 
   if (limit > 0)
   {
-    var irt = TryASA(Iterate(S1, TV.Contents.Side), limit: limit - 1);
-    if (irt is Tri.Triangle)
+    /*var irt =*/ TrySAS(Iterate(S1, TV.Contents.Side), limit: limit - 1);
+    /*if (irt is Tri.Triangle)
     {
       irt.RotAaB();
-    }
+    }*/
+
   }
   return null;
 }
@@ -179,12 +226,13 @@ Tri.Triangle? TrySSS()
 
 void TrySolve()
 {
-  print("Trying to Solve");
-  Tri.Triangle? tri = TryAAS(0);
-  tri ??= TryASA(0);
-  tri ??= TrySSA(5);
-  tri ??= TrySAS(5);
-  tri ??= TrySSS();
+  Tri.Triangle? tri = TryAAS(0); //001
+  tri ??= TryASA(0); //010
+  tri ??= TryASS(0); //011
+  tri ??= TrySAA(5); //100
+  tri ??= TrySAS(5); //101
+  tri ??= TrySSA(5); //110
+  tri ??= TrySSS();  //111
 
   if (tri != null)
   {
@@ -200,11 +248,6 @@ void TrySolve()
 
 void ToPrimitive2D(int intex)
 {
-  //TrySolve();
-  if(TV.TxtControllers[intex].text.isEmpty)
-  {
-    //print("$intex is empty");
-  }
   try
   {
     TV.Values[intex] = null;
@@ -240,17 +283,15 @@ void ToPrimitive2D(int intex)
 
 Scaffold TriSolver(BuildContext context)
 {
-  for(int fv = 0; fv <= 2; fv ++)
+  for(int fv = 0; fv <= 5; fv++) {TV.Values[fv] = null;}
+  for(int fv = 0; fv <= 5; fv++)
   {
-    TV.Values[fv] = null;
+    String txt =  TV.TxtControllers[fv].text;
     (TV.TxtControllers[fv].text.isEmpty) ? null : ToPrimitive2D(fv);
   }
-  TrySolve();
   print(TV.Values);
-  for (var box in TV.TxtControllers)
-  {
-   // print(box.text);
-  }
+  TrySolve();
+  int fv = 0;
 	return Scaffold(body: 
 		Column(children:
 			[
